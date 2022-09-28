3c5cba8f5bdc5426aeb2472f78e9ad8ee3264b53
Dương Việt Phát <phatduongvgt@gmail.com>
2022-09-28 16:00:55 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -13,4 +15,17 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+
 module.exports = app;
