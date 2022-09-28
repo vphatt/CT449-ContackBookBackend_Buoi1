86a8b2071335a73099f9d5b004bcf244b575ac25
Dương Việt Phát <phatduongvgt@gmail.com>
2022-09-28 15:59:08 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
+
 const app = express();
 
 app.use(cors());
@@ -10,5 +12,5 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application"});
 });
 
-
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
